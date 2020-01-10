f8a11145dcc9af8ede47b065df0e27a4fb2fec31
Coleman Shropshire <CS053196@psd.provo.edu>
2020-01-10 08:59:30 -0700

    so anyways, im plant

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import "./styles/main.scss";
 import { hot } from "react-hot-loader";
+import "../styles/main.scss";
+
+import Homepage from "./pages/home.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, World! </h1>
+        <Homepage />
       </div>
     );
   }
---
+++ src/components/pages/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Homepage extends Component {
+  render() {
+    return(
+      <div className="homepage-wrapper" >
+        <p>hey</p>
+      </div>
+    )
+  }
+}
